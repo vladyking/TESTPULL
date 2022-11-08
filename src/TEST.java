c52848606f344cc2bc72b31ae00097cfa9d61c63
vladyking <gvladykin@gmail.com>
2022-11-08 21:30:55 +0300

    All done

--- src/TEST.java
+++ src/TEST.java
@@ -3,4 +3,7 @@ public class TEST {
     public static String text () {
         return text();
     }
+
+    public TEST() {
+    }
 }
--- src/Template.java
+++ src/Template.java
@@ -1,2 +1,16 @@
 public class Template {
+    private String Ant;
+
+    public Template(String ant) {
+        Ant = ant;
+    }
+
+    public String getAnt() {
+        return Ant;
+    }
+
+    public void setAnt(String ant) {
+        Ant = ant;
+    }
+
 }
